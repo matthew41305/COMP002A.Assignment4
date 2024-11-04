dfb8eeb4832507a353eba969a9eec4582a8daf6d
Matthew <matthew41305@gmail.com>
2024-11-03 19:06:11 -0800

    transfered zip files and did calculation to find circumference when given the radius, matched with numbers from example

--- script.js
+++ script.js
@@ -5,9 +5,21 @@
 
 // 2. Write a function that takes the radius of a circle and outputs it’s approximate circumference (the formula for circumference is 2 *pi * radius, you may use 3.14 for pi).
 // Example Output:
+// Function that calculates the circumference of a circle given its radius
 // console.log(calculateCircumference(5)); // will output 31.4
+function calculateCircumference(radius) {
+    const pi = 3.14;
+    return 2 * pi * radius;
+  }
+  // Example Output:
+  console.log(calculateCircumference(5)); // 31.4
+  
 
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
 // console.log(searchString(“q”, “mississippi”)); // will output 0
+
+// Function that returns how many times a character appears in a string
+
+  
